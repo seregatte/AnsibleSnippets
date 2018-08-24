a6a22a9f4984e0442f946482fbb126334fd7df87
João Paulo Seregatte Costa <seregatte@gmail.com>
2018-08-24 19:13:37 -0300

    Git process order

--- .travis/develop.sh
+++ .travis/develop.sh
@@ -3,11 +3,12 @@ sudo apt-get update > /dev/null
 sudo apt-get install git-core --no-install-recommends --no-install-suggests -y
 git config --global user.email "seregatte@gmail.com"
 git config --global user.name "João Paulo Seregatte Costa"
+git fetch --all
 docker build generator/ -t builder:latest
 export ANSIBLE_VERSION=`docker run --rm -it builder:latest ansible --version | head -1 | cut -d' ' -f2`
+git checkout -b ${CI_BRANCH} || git checkout ${CI_BRANCH}
 docker run --rm -it -v `pwd`:/var/www/html builder:latest make
 export CI_BRANCH=`date +%Y-%m-%d`-ci
-git checkout -b ${CI_BRANCH} || git checkout ${CI_BRANCH}
 git add . *.sublime-snippet
 git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER} for ${ANSIBLE_VERSION}"
 git remote add origin-ci https://${GITHUB_TOKEN}@github.com/seregatte/AnsibleSnippets.git > /dev/null 2>&1
