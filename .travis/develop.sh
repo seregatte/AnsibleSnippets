5682ffddb0450800f3d2425d9a6f293029b270bf
João Paulo Seregatte Costa <seregatte@gmail.com>
2018-08-24 20:16:59 -0300

    Git process flow

--- .travis/develop.sh
+++ .travis/develop.sh
@@ -6,10 +6,13 @@ git config --global user.name "João Paulo Seregatte Costa"
 git fetch --all
 docker build generator/ -t builder:latest
 export ANSIBLE_VERSION=`docker run --rm -it builder:latest ansible --version | head -1 | cut -d' ' -f2`
+export CI_BRANCH=`date +%Y-%m-%d`-ci
+export CI_NEXT_TAG=$(printf '%s.%0d' `git tag -l | tail -1 | cut -d'.' -f1-2` $((`git tag -l | tail -1 | cut -d'.' -f3`+1)))
 git checkout -b ${CI_BRANCH} || git checkout ${CI_BRANCH}
 docker run --rm -it -v `pwd`:/var/www/html builder:latest make
-export CI_BRANCH=`date +%Y-%m-%d`-ci
 git add . *.sublime-snippet
+sed -Ei -- "s%(Ansible\ Snippets).*%\1 $CI_NEXT_TAG%" README.md
+git add README.md
 git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER} for ${ANSIBLE_VERSION}"
 git remote add origin-ci https://${GITHUB_TOKEN}@github.com/seregatte/AnsibleSnippets.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-ci ${CI_BRANCH}
--- .travis/master.sh
+++ .travis/master.sh
@@ -3,7 +3,7 @@ sudo apt-get update > /dev/null
 sudo apt-get install git-core --no-install-recommends --no-install-suggests -y
 git config --global user.email "seregatte@gmail.com"
 git config --global user.name "João Paulo Seregatte Costa"
-git tag -a $(printf '%s.%0d' `git tag -l | tail -1 | cut -d'.' -f1-2` \
-	$((`git tag -l | tail -1 | cut -d'.' -f3`+1))) -m "${TRAVIS_COMMIT_MESSAGE}"
+export CI_NEXT_TAG=$(printf '%s.%0d' `git tag -l | tail -1 | cut -d'.' -f1-2` $((`git tag -l | tail -1 | cut -d'.' -f3`+1)))
+git tag -a ${CI_NEXT_TAG} -m "${TRAVIS_COMMIT_MESSAGE}"
 git remote add origin-ci https://${GITHUB_TOKEN}@github.com/seregatte/AnsibleSnippets.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-ci --tags
