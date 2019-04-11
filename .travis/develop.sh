b41c8862fd389e134f454ca26fa0a8486f71c9a3
João Paulo Seregatte Costa <seregatte@gmail.com>
2019-04-11 17:24:20 -0300

    Fix number version - debug

--- .travis/develop.sh
+++ .travis/develop.sh
@@ -10,9 +10,10 @@ export CI_BRANCH=`date +%Y-%m-%d`-ci
 export CI_NEXT_TAG=$(printf '%s.%0d' `git tag -l | tail -1 | cut -d'.' -f1-2` $((`git tag -l | tail -1 | cut -d'.' -f3`+1)))
 git checkout -b ${CI_BRANCH} || git checkout ${CI_BRANCH}
 git pull --rebase origin ${CI_BRANCH} && true
-docker run --rm -it -v `pwd`:/var/www/html builder:latest make
+#docker run --rm -it -v `pwd`:/var/www/html builder:latest make
 git add . *.sublime-snippet
 head -1 README.md
+echo ${CI_NEXT_TAG}
 mv README.md README.md.tpl; cat README.md.tpl | sed -E "s%(Ansible\ Snippets).*%\1 ${CI_NEXT_TAG}%" > README.md ; rm README.md.tpl
 head -1 README.md
 echo "git diff" && git diff
